bf30b9b06fef835fd3c2c05c90e74d46c77d398b
Sun Yitao <sunyitao8@gmail.com>
2019-05-26 12:07:21 +0800

    add lib

--- code/run_predictions.py
+++ code/run_predictions.py
@@ -11,8 +11,16 @@ from keras import backend as K
 
 from lib.adabound import AdaBound
 
-MODEL_PATH = ''
+MODEL_PATH = '/Users/sunyitao/Documents/Projects/ML_Projects/GrabAIChallenge/data/checkpoints/baseline_cnn/Xception_Imagenet/model.60-0.94.h5'
 IMAGE_SIZE = (363, 525)  # height, width
+
+os.environ['KERAS_BACKEND'] = 'plaidml.keras.backend'
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+K.set_session(session)
 
 
 def get_input_data_generators_from_directory(test_directory, batch_size=32):
@@ -41,5 +49,5 @@ def get_input_data_generator_from_csv(csv_path, test_directory=None, x_col='file
 if __name__ == '__main__':
     model = keras.models.load_model(
         MODEL_PATH, custom_objects={'AdaBound': AdaBound})
-    test = get_input_data_generators_from_directory('', batch_size=32)
-    model.predict_generator
+    test = get_input_data_generators_from_directory('/Users/sunyitao/Documents/Projects/ML_Projects/GrabAIChallenge/data/stanford-car-dataset-by-classes-folder/car_data/new_data_cleaned', batch_size=32)
+    model.evaluate_generator(test, steps=len(test), verbose=1)
