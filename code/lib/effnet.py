17cf79b94233327396034e4c882856e187b9f452
Yitao Sun <yitao.sun@shopee.com>
2019-05-29 13:53:21 +0800

    deepaug effnet

--- code/lib/effnet.py
+++ code/lib/effnet.py
@@ -1,51 +1,51 @@
 from keras.models import Model
-from keras.layers import *
-from keras.activations import *
-from keras.callbacks import *
+from keras import layers
 
 
 def get_post(x_in):
-    x = LeakyReLU()(x_in)
-    x = BatchNormalization()(x)
+    x = layers.LeakyReLU()(x_in)
+    x = layers.BatchNormalization()(x)
     return x
 
+
 def get_block(x_in, ch_in, ch_out):
-    x = Conv2D(ch_in,
-               kernel_size=(1, 1),
-               padding='same',
-               use_bias=False)(x_in)
+    x = layers.Conv2D(ch_in,
+                      kernel_size=(1, 1),
+                      padding='same',
+                      use_bias=False)(x_in)
     x = get_post(x)
 
-    x = DepthwiseConv2D(kernel_size=(1, 3), padding='same', use_bias=False)(x)
+    x = layers.DepthwiseConv2D(kernel_size=(
+        1, 3), padding='same', use_bias=False)(x)
     x = get_post(x)
-    x = MaxPool2D(pool_size=(2, 1),
-                  strides=(2, 1))(x) # Separable pooling
+    x = layers.MaxPool2D(pool_size=(2, 1),
+                         strides=(2, 1))(x)  # Separable pooling
 
-    x = DepthwiseConv2D(kernel_size=(3, 1),
-                        padding='same',
-                        use_bias=False)(x)
+    x = layers.DepthwiseConv2D(kernel_size=(3, 1),
+                               padding='same',
+                               use_bias=False)(x)
     x = get_post(x)
 
-    x = Conv2D(ch_out,
-               kernel_size=(2, 1),
-               strides=(1, 2),
-               padding='same',
-               use_bias=False)(x)
+    x = layers.Conv2D(ch_out,
+                      kernel_size=(2, 1),
+                      strides=(1, 2),
+                      padding='same',
+                      use_bias=False)(x)
     x = get_post(x)
 
     return x
 
 
 def Effnet(input_shape, nb_classes, include_top=True, weights=None):
-    x_in = Input(shape=input_shape)
+    x_in = layers.Input(shape=input_shape)
 
     x = get_block(x_in, 32, 64)
     x = get_block(x, 64, 128)
     x = get_block(x, 128, 256)
 
     if include_top:
-        x = Flatten()(x)
-        x = Dense(nb_classes, activation='softmax')(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(nb_classes, activation='softmax')(x)
 
     model = Model(inputs=x_in, outputs=x)
 
