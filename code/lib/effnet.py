4686db02d5ab7f47cb14e7e5e811be0207cd46d7
Yitao Sun <yitao.sun@shopee.com>
2019-05-29 14:42:44 +0800

    deepaug effnet

--- code/lib/effnet.py
+++ code/lib/effnet.py
@@ -8,40 +8,45 @@ def get_post(x_in):
     return x
 
 
-def get_block(x_in, ch_in, ch_out):
+def get_block(x_in, ch_in, ch_out, regularizer):
     x = layers.Conv2D(ch_in,
                       kernel_size=(1, 1),
                       padding='same',
-                      use_bias=False)(x_in)
+                      use_bias=False,
+                      kernel_regularizer=regularizer)(x_in)
     x = get_post(x)
 
-    x = layers.DepthwiseConv2D(kernel_size=(
-        1, 3), padding='same', use_bias=False)(x)
+    x = layers.DepthwiseConv2D(kernel_size=(1, 3), 
+                               padding='same', 
+                               use_bias=False,
+                               depthwise_regularizer=regularizer)(x)
     x = get_post(x)
     x = layers.MaxPool2D(pool_size=(2, 1),
                          strides=(2, 1))(x)  # Separable pooling
 
     x = layers.DepthwiseConv2D(kernel_size=(3, 1),
                                padding='same',
-                               use_bias=False)(x)
+                               use_bias=False,
+                               depthwise_regularizer=regularizer)(x)
     x = get_post(x)
 
     x = layers.Conv2D(ch_out,
                       kernel_size=(2, 1),
                       strides=(1, 2),
                       padding='same',
-                      use_bias=False)(x)
+                      use_bias=False,
+                      kernel_regularizer=regularizer)(x)
     x = get_post(x)
 
     return x
 
 
-def Effnet(input_shape, nb_classes, include_top=True, weights=None):
+def Effnet(input_shape, nb_classes, include_top=True, weights=None, regularizer=1e-4):
     x_in = layers.Input(shape=input_shape)
 
-    x = get_block(x_in, 32, 64)
-    x = get_block(x, 64, 128)
-    x = get_block(x, 128, 256)
+    x = get_block(x_in, 32, 64, regularizer)
+    x = get_block(x, 64, 128, regularizer)
+    x = get_block(x, 128, 256, regularizer)
 
     if include_top:
         x = layers.Flatten()(x)
