773e8ca5a034d79299ba026f8af56f5e25659a8f
Sun Yitao <sunyitao8@gmail.com>
2019-06-16 22:09:20 +0800

    fix cuda tensor

--- ws-dan/wsdan_predict.py
+++ ws-dan/wsdan_predict.py
@@ -8,6 +8,7 @@ from multiprocessing import cpu_count
 
 import numpy as np
 import pandas as pd
+from scipy.special import softmax
 from PIL import Image
 from sklearn.metrics import precision_recall_fscore_support
 from tqdm import tqdm
@@ -150,8 +151,6 @@ def predict_class_probabilities(options):
 
     start_time = time.time()
     net.eval()
-    y_pred_raw_numpy = np.zeros((len(dataset), 196))
-    y_pred_crop_numpy = np.zeros((len(dataset), 196))
     y_pred_average = np.zeros((len(dataset), 196))
 
     with torch.no_grad():
@@ -182,8 +181,6 @@ def predict_class_probabilities(options):
 
             y_pred = (y_pred_raw + y_pred_crop) / 2
             y_pred_average[i*options.batch_size:(i+1)*options.batch_size] = y_pred.cpu().numpy()
-            y_pred_raw_numpy[i*options.batch_size:(i+1)*options.batch_size] = y_pred_raw.cpu().numpy()
-            y_pred_crop_numpy[i*options.batch_size:(i+1)*options.batch_size] = y_pred_crop.cpu().numpy()
             batches += 1
             if options.do_eval:
                 # loss
@@ -202,7 +199,7 @@ def predict_class_probabilities(options):
         ground_truth = [sample[1] for sample in dataset.samples]
         precision, recall, f1, _ = precision_recall_fscore_support(ground_truth, np.argmax(y_pred_average, axis=1), average='micro')
         logging.info(f'Precision: {precision}, Recall: {recall}, Micro F1: {f1}')
-        y_pred_average = F.softmax(y_pred_average, dim=1)
+        y_pred_average = softmax(y_pred_average, axis=1)
         save_predictions(image_list, y_pred_average, options, ground_truth=ground_truth)
     else:
         save_predictions(image_list, y_pred_average, options)
