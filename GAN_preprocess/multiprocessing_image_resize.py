fe8a80759864872845379b9037e8af8ab1778819
Yitao Sun <yitao.sun@shopee.com>
2019-06-17 10:40:21 +0800

    add super res

--- GAN_preprocess/multiprocessing_image_resize.py
+++ GAN_preprocess/multiprocessing_image_resize.py
@@ -7,7 +7,7 @@ data_dir = Path.cwd().parent / 'data' / 'downsampled'
 
 def resize_image(input_path):
     im = cv2.imread(input_path)
-    ratio = 128 / min(im.shape)
+    ratio = 128 / min(im.shape[:2])
     small_im = cv2.resize(im, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_AREA)
     print(small_im.shape)
     cv2.imwrite(input_path, small_im)
